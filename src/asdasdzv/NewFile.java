87cd65641593421c8d16a1096a1d140aa8316d1d
skc05096@naver.com <skc05096@naver.com>
2025-01-27 09:59:01 +0900

    1. 테스트자바 주석추가 , 2.NewFile자바파일 추가

---
+++ src/asdasdzv/NewFile.java
@@ -0,0 +1,5 @@
+package asdasdzv;
+
+public class NewFile {
+	//새로운 파일추가//
+}
--- src/asdasdzv/test.java
+++ src/asdasdzv/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// test
-
+		// 소스트리 활용 / 지속적활용 / 
 	}
 
 }
